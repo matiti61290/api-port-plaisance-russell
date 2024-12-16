afa44023dfa82d0163ea4d4006216a9990029510
Mathieu <mathieu.barbey18@gmail.com>
2024-12-16 16:36:08 +0100

    authentification via home

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,13 @@ const router = express.Router();
 const userRoute = require('../routes/users');
 
 router.get('/', async (req, res) => {
-  res.status(200).json({
-    name:process.env.APP_NAME,
-    version: '1.0',
-    status: 200,
-    message: "Bienvenue sur l'API !"
-  });
+  // res.status(200).json({
+  //   name:process.env.APP_NAME,
+  //   version: '1.0',
+  //   status: 200,
+  //   message: "Bienvenue sur l'API !"
+  // });
+  res.render('home', {errorMessage: null});
 });
 
 router.use('/users', userRoute);
